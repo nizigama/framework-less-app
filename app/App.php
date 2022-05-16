a7533a062787bb91e92ddb960ad3b7c0e16d36a2
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-16 21:00:42 +0200

    Created the app class representing the whole app

---
+++ app/App.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App;
+
+use DateTime;
+
+class App
+{
+    public const VERSION = 1.0;
+
+    public function __construct(string $environment = "development")
+    {
+        // connect to database
+
+        $this->errorLogging($environment);
+    }
+
+    private function errorLogging(string $environment): void
+    {
+        $errorFile = fopen(dirname(__DIR__) .  "/logs/" . $environment . "/errors.log", "a+");
+
+        if ($environment === "production") {
+            error_reporting(~E_ALL);
+            ini_set('display_errors', "0");
+        } else {
+            error_reporting(E_ALL);
+            ini_set('display_errors', "1");
+        }
+
+
+        register_shutdown_function(function () use ($errorFile) {
+
+            if (count($err = error_get_last()) > 0) {
+
+                $timestamp = (new DateTime())->format("Y-m-d, H:i:s");
+                $type = $err['type'];
+                $line = $err['line'];
+                $message = $err['message'];
+
+                fwrite($errorFile, "datetime: {$timestamp}\ntype: $type\nline: $line\nmessage: $message\n\n");
+            }
+
+            fclose($errorFile);
+        });
+    }
+}

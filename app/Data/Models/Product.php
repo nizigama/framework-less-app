f03b92dcae4780a22ec824e26c8c5f8670a7fc6b
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-26 23:56:44 +0200

    Created models to link with the database

---
+++ app/Data/Models/Product.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Models;
+
+use App\Data\Model;
+use PDO;
+
+final class Product extends Model
+{
+    protected int $id;
+    protected string $name;
+    protected string $sku;
+    protected float $price;
+    protected int $typeID;
+    protected ?string $typeName = null;
+    protected ?float $size = null;
+    protected ?float $weight = null;
+    protected ?float $height = null;
+    protected ?float $width = null;
+    protected ?float $length = null;
+
+    public function __construct(int $id, string $name, string $sku, float $price, int $typeID, float $size = null, float $weight = null, float $height = null, float $width = null, float $length = null)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->sku = $sku;
+        $this->price = $price;
+        $this->typeID = $typeID;
+        $this->size = $size;
+        $this->weight = $weight;
+        $this->height = $height;
+        $this->width = $width;
+        $this->length = $length;
+    }
+
+    /**
+     * @return Product[]
+     */
+    public static function all()
+    {
+        $stmt = self::$db->prepare("SELECT * FROM Product");
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_FUNC, [__CLASS__, "createType"]);
+
+        return self::readable($results);
+    }
+
+    private static function createType(string $id, string $name, string $sku, string $price, string $typeID, string $size = null, string $weight = null, string $height = null, string $width = null, string $length = null)
+    {
+        return new self(intval($id), $name, $sku, floatval($price), intval($typeID), floatval($size), floatval($weight), floatval($height), floatval($width), floatval($length));
+    }
+
+    private static function readable(array $products): array
+    {
+        $types = ProductType::all();
+
+        return array_map(function (Product $product) use ($types) {
+            $filteredType = array_filter($types, function (ProductType $type) use ($product) {
+                return $type->getId() === $product->typeID;
+            });
+
+            $product->typeName = [...$filteredType][0]->getName();
+            return $product;
+        }, $products);
+    }
+}
---
+++ app/Data/Models/ProductType.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Data\Models;
+
+use App\Data\Model;
+use PDO;
+
+final class ProductType extends Model
+{
+    protected int $id;
+    protected string $name;
+
+    public function __construct(int $id, string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+
+    /** 
+     * Get all product type records from the database
+     * 
+     * @return ProductType[] 
+     */
+    public static function all()
+    {
+        $stmt = self::$db->prepare("SELECT * FROM ProductType");
+
+        $stmt->execute();
+
+        $results = $stmt->fetchAll(PDO::FETCH_FUNC, [__CLASS__, "createType"]);
+
+        return $results;
+    }
+
+    /** 
+     * Get a product type record from the database by its ID
+     * 
+     */
+    public static function findByID(int $id): ?ProductType
+    {
+        $stmt = self::$db->prepare("SELECT * FROM ProductType WHERE id = ?");
+
+        $stmt->execute([$id]);
+
+        $result = $stmt->fetch(PDO::FETCH_OBJ);
+
+        return is_bool($result) ? null : self::createType($result->id, $result->type);
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    private static function createType(string $id, string $name)
+    {
+        return new self((int)$id, $name);
+    }
+}

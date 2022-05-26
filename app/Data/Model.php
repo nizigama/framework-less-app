c7e32399de03b9cc6dff45d7cfe4da215f2e4178
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-26 22:55:17 +0200

    Made the base model serializable to allow json decode to access protected properties of models

--- app/Data/Model.php
+++ app/Data/Model.php
@@ -5,14 +5,30 @@ declare(strict_types=1);
 namespace App\Data;
 
 use App\Services\Logger;
+use JsonSerializable;
 use PDO;
+use stdClass;
 
-abstract class Model
+abstract class Model implements JsonSerializable
 {
     protected static PDO $db;
 
     public static function loadDB(Logger $logger)
     {
         self::$db = DB::getInstance($logger)->getDB();
+    }
+
+    public function jsonSerialize()
+    {
+        $obj = new stdClass();
+
+        $data = get_object_vars($this);
+        $properties = array_keys(get_object_vars($this));
+
+        foreach ($properties as $property) {
+            $obj->$property = $data[$property];
+        }
+
+        return $obj;
     }
 }

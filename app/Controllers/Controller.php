0444c7c52efe44662c98f2801f78d0d5d8cdafb7
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-26 20:13:00 +0200

    Created default controller for index page and added example handler with validation

---
+++ app/Controllers/Controller.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controllers;
+
+use App\Http\Request;
+use App\Http\Response;
+use App\Services\Validation;
+
+class Controller
+{
+    public function index(Request $request, ...$queryParameters)
+    {
+
+
+        Response::response(["status" => "Up and running"]);
+    }
+
+    // public function example_with_validation(Request $request, ...$queryParameters)
+    // {
+        // $validation = Validation::make($request->data(), [
+        //     "input" => ["rule1", "rule2"]
+        // ]);
+
+        // if ($validation->failed) {
+        //  Response::response(["errors" => $validation->errorMessages],[],Response::BAD_REQUEST);
+        // }
+    // }
+}

e14683c7f059d3f280a9e5abe7295d49e1534dce
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-26 20:11:13 +0200

    Improvement and house cleaning

--- app/Http/Response.php
+++ app/Http/Response.php
@@ -6,23 +6,27 @@ namespace App\Http;
 
 class Response
 {
-    public static function json(): self
+    public const BAD_REQUEST = 400;
+    public const OK = 200;
+
+    private static function json()
     {
         self::setHeader(["Content-Type" => "application/json"]);
-
-        return static ;
     }
 
-    public static function response(array $data, array $headers)
+    public static function response(array $data, array $headers = [], int $statusCode = self::OK)
     {
+        self::json();
+
         foreach ($headers as $header) {
             self::setHeader($header);
         }
 
+        http_response_code($statusCode);
         echo json_encode($data);
     }
 
-    public static function setHeader(array $header): void
+    private static function setHeader(array $header): void
     {
         $type = array_key_first($header);
         $value = array_values($header)[0];

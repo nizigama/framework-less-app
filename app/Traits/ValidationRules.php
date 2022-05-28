84fdaee4d49df165935b7760cf84f66ec2f294d2
Nizigama jean davy <jedavy.n@gmail.com>
2022-05-28 13:50:10 +0200

    Added more validation rules

--- app/Traits/ValidationRules.php
+++ app/Traits/ValidationRules.php
@@ -9,22 +9,50 @@ use App\Data\Models\ProductType;
 
 trait ValidationRules
 {
-    protected static function required(string $value, string $field): ?string
+    protected static function required(mixed $value, string $field): ?string
     {
-        return trim($value) !== "" ? null : "$field cannot be empty";
+        if (is_string($value)) {
+            $value = trim($value);
+        }
+        return empty($value) !== "" ? null : "$field cannot be empty";
     }
 
-    protected static function numeric(string $value, string $field): ?string
+    protected static function numeric(mixed $value, string $field): ?string
     {
         return is_numeric($value) ? null : "$field is not a valid numeric value";
     }
 
-    protected static function uniqueSku(string $value, string $field): ?string
+    protected static function arrayOfNumbers(mixed $value, string $field): ?string
+    {
+
+        if (!is_array($value)) {
+            return "$field is not a valid array";
+        }
+
+        foreach ($value as $item) {
+
+            if (!is_numeric($item)) {
+
+                return "$item is not a valid numeric value";
+            }
+        }
+
+        return null;
+    }
+
+    protected static function uniqueSku(mixed $value, string $field): ?string
     {
         return !Product::skuAlreadyUsed($value) ? null : "$field has already been used by another product as SKU";
     }
 
-    protected static function typeExists(string $value, string $field): ?string
+    protected static function productsExist(mixed $value, string $field): ?string
+    {
+        $productsRetrieved = Product::getProductsByIDs($value);
+        $IDsGiven = $value;
+        return count($IDsGiven) === count($productsRetrieved) ? null : "$field has products that have not been found";
+    }
+
+    protected static function typeExists(mixed $value, string $field): ?string
     {
         $type = ProductType::findByID(intval($value));
         return !is_null($type) ? null : "$field is not a valid type";
